3758164d6e27ba94f76925ba135a935e76437d76
skittlepox <benjamin_spiegel@brown.edu>
2020-11-02 22:32:24 -0500

    first commit

---
+++ data_generator.py
@@ -0,0 +1,71 @@
+import random
+import numpy as np
+
+
+def generate_samples(count: int) -> [[], []]:
+    samples = []
+    for i in range(count):
+        vx0 = random.uniform(5, 10)
+        dy0 = random.uniform(-10, 10)
+        vy0 = 50 * 1 / vx0 + dy0 * 1 / 10 * vx0
+
+        # pick a language command vector
+        intensity = random.sample([-3, -2, -1, 1, 2, 3], 1)[0]
+        if bool(random.getrandbits(1)):
+            # apply intensity to vx (faster)
+            vx1 = random.gauss((vx0 + intensity), 0.1)
+            dy1 = dy0
+            vy1 = 50 * 1 / vx1 + dy1 * 1 / 10 * vx1
+            c = [intensity, 0]
+        else:
+            # apply intensity to dy (higher)
+            vx1 = vx0
+            dy1 = random.gauss((dy0 + intensity), 0.1)
+            vy1 = 50 * 1 / vx1 + dy1 * 1 / 10 * vx1
+            c = [0, intensity]
+
+        sample = [[vx0, vy0, c[0], c[1]], [vx1, vy1]]
+        samples.append(sample)
+    return samples
+
+
+def calculate_normalization_values(samples: []) -> (np.array, np.array):
+    inputs = [s[0] for s in samples]
+    inputs = np.array(inputs, dtype=np.float32)
+    mu = np.sum(inputs, axis=0) / len(samples)
+    sigma2 = np.sum((inputs - mu)**2, axis=0) / len(samples)
+    return mu, sigma2
+
+
+def normalize_samples(samples: [], mu: np.array, sigma2: np.array) -> []:
+    inputs = [s[0] for s in samples]
+    outputs = [s[1] for s in samples]
+    inputs = np.array(inputs, dtype=np.float32)
+    outputs = np.array(outputs, dtype=np.float32)
+
+    inputs -= mu
+    inputs /= sigma2
+    outputs -= mu[:2]
+    outputs /= sigma2[:2]
+
+    new_samples = np.array([np.concatenate((i, o)) for i, o in zip(inputs, outputs)])
+    return new_samples
+
+
+def main():
+    samples_train = generate_samples(20000)
+    samples_test = generate_samples(2000)
+    mu, sigma2 = calculate_normalization_values(samples_train)
+
+    np.savetxt("data/mu.txt", mu)
+    np.savetxt("data/sigma2.txt", sigma2)
+
+    new_samples_train = normalize_samples(samples_train, mu, sigma2)
+    new_samples_test = normalize_samples(samples_test, mu, sigma2)
+
+    np.savetxt("data/training.txt", new_samples_train)
+    np.savetxt("data/testing.txt", new_samples_test)
+
+
+if __name__ == "__main__":
+    main()
---
+++ net.py
@@ -0,0 +1,95 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.dense1 = nn.Linear(in_features=4, out_features=10)
+        self.dense2 = nn.Linear(in_features=10, out_features=10)
+        self.dense3 = nn.Linear(in_features=10, out_features=2)
+
+    def forward(self, x):
+        x = self.dense1(x)
+        x = F.relu(x)
+        x = self.dense2(x)
+        x = F.relu(x)
+        x = self.dense3(x)
+        return x
+
+
+def test(net: Net):
+    samples = np.loadtxt("data/testing.txt", dtype=np.float32)
+    inputs = torch.tensor(samples[:, :4], requires_grad=False)
+    outputs = torch.tensor(samples[:, -2:], requires_grad=False)
+
+    criterion = nn.MSELoss()
+
+    predictions = net.forward(inputs)
+    loss = criterion(predictions, outputs)
+    print(f"Testing Loss: {loss}")
+
+
+def train(net: Net):
+    samples = np.loadtxt("data/training.txt", dtype=np.float32)
+    inputs = torch.tensor(samples[:, :4], requires_grad=True)
+    outputs = torch.tensor(samples[:, -2:], requires_grad=True)
+
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.1)
+
+    for _ in range(1000):
+        optimizer.zero_grad()
+        predictions = net.forward(inputs)
+        loss = criterion(predictions, outputs)
+        loss.backward()
+        optimizer.step()
+        print(f"Training Loss: {loss}")
+        test(net)
+
+
+def denormalize(predictions: torch.Tensor, mu: np.array, sigma2: np.array) -> np.array:
+    predictions = predictions.detach().numpy()
+    predictions *= sigma2[:-2]
+    predictions += mu[:-2]
+    return predictions
+
+
+def normalize(samples: np.array, mu: np.array, sigma2: np.array) -> np.array:
+    samples -= mu
+    samples /= sigma2
+    return torch.tensor(samples)
+
+
+def main():
+    net = Net()
+    test(net)
+    train(net)
+    torch.save(net.state_dict(), "data/model.pt")
+
+
+def repl():
+    net = Net()
+    net.load_state_dict(torch.load("data/model.pt"))
+    net.eval()
+
+    mu = np.loadtxt("data/mu.txt")
+    sigma2 = np.loadtxt("data/sigma2.txt")
+
+    while True:
+        s = input("input: ").split()
+        arr = np.array(s, dtype=np.float32)
+        arr_normed = normalize(arr, mu, sigma2)
+        pred = net.forward(arr_normed)
+        denormalize(pred, mu, sigma2)
+        print(pred)
+
+
+if __name__ == "__main__":
+    # main()
+    repl()
