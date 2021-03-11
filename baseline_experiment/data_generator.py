bdd11fa98e42eec290f184bf81c38542a75e3f77
skittlepox <benjamin_spiegel@brown.edu>
2021-03-10 23:22:13 -0500

    reworked data generator

--- baseline_experiment/data_generator.py
+++ baseline_experiment/data_generator.py
@@ -1,76 +1,49 @@
 import random
 import numpy as np
+from collections.abc import Callable
+from sentence_transformers import SentenceTransformer
+
+sbert_model = SentenceTransformer('bert-base-nli-mean-tokens')
 
 
-def skill(tau: []) -> []:
-    x_vel = tau[0]
+def ball_launch(tau: np.array) -> np.array:
+    time = tau[0]
     y_pos = tau[1]
 
-    y_vel = 50 * 1 / x_vel + y_pos * 1 / 10 * x_vel
+    x_vel = 10.0/time
+    y_vel = 5.0 * time + y_pos / time
 
-    theta = [x_vel, y_vel]
+    theta = np.array([x_vel, y_vel])
     return theta
 
 
-def generate_samples(count: int) -> []:
+def label_ball_launch(theta: np.array, theta_prime: np.array) -> str:
+    delta = theta_prime - theta
+    label = ""
+    # TODO estimate adverb changes
+    if abs(delta[0]) > 0.5:
+        pass
+    return delta
+
+
+def generate_samples(count: int, skill: Callable, task_min: [], task_max: [], task_perturb_diff: np.array) -> np.array:
     samples = []
     for i in range(count):
         # sample random tau
-        x_vel = random.uniform(5, 10)
-        y_pos = random.uniform(-10, 10)
-        tau = np.array([x_vel, y_pos])
+        tau = random.uniform(task_min, task_max)
+        theta = skill(tau)
 
-        # pick a lambda and its corresponding delta_tau
-        delta = random.sample([-3. -2, -1, 0, 1, 2, 3], 1)[0]
-        if bool(random.getrandbits(1)):
-            # apply delta to x velocity
-            x_vel2 = random.gauss((x_vel + delta), 0.1) # for fuzziness
-            y_pos2 = y_pos
-            lam = np.array([delta, 0])
-        else:
-            # apply delta to y position
-            x_vel2 = x_vel
-            y_pos2 = random.gauss((y_pos + delta), 0.1)
-            lam = np.array([0, delta])
-        tau2 = np.array([x_vel2, y_pos2])
-        delta_tau = tau2 - tau
-        samples.append(np.concatenate([tau, lam, delta_tau]))
-    return samples
+        tau_prime = random.uniform(tau - task_perturb_diff, tau + task_perturb_diff)
+        tau_prime = np.clip(tau_prime, task_min, task_max)
+        theta_prime = skill(tau_prime)
+
+        samples.append(np.concatenate([tau, theta, tau_prime, theta_prime]))
+    return np.array(samples)
 
 
-def generate_samples_uniform(count: int) -> []:
-    samples = []
-
-    for i in range(count):
-        vx0 = random.uniform(5, 10)
-        dy0 = random.uniform(-10, 10)
-        vy0 = 50 * 1 / vx0 + dy0 * 1 / 10 * vx0
-
-        if bool(random.getrandbits(1)):
-            # apply intensity to vx (faster)
-            vx1 = random.uniform(vx0 - 2.5, vx0 + 2.5)
-            dy1 = dy0
-            vy1 = 50 * 1 / vx1 + dy1 * 1 / 10 * vx1
-            if vx1 > vx0:
-                intensity = 1
-            else:
-                intensity = -1
-            c = [intensity, 0]
-        else:
-            # apply intensity to dy (higher)
-            vx1 = vx0
-            dy1 = random.uniform(dy0 - 10, dy0 + 10)
-            vy1 = 50 * 1 / vx1 + dy1 * 1 / 10 * vx1
-
-            if dy1 > dy0:
-                intensity = 1
-            else:
-                intensity = -1
-            c = [0, intensity]
-
-        sample = [vx0, vy0, c[0], c[1], vx1, vy1]
-        samples.append(sample)
-    return samples
+def label_samples(samples: np.array, score: Callable) -> np.array:
+    labels = [label_ball_launch(s[2:4], s[6:8]) for s in samples]
+    return labels
 
 
 def calculate_normalization_values(samples: []) -> (np.array, np.array):
@@ -97,7 +70,7 @@ def normalize_samples(samples: [], mu: np.array, sigma2: np.array) -> []:
 
 
 def main():
-    data_label = "v0"
+    data_label = "v1"
     samples_train = generate_samples(20000)
     samples_test = generate_samples(2000)
 
@@ -117,4 +90,15 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    samples = generate_samples(20, ball_launch, task_min=np.array([1, -15]), task_max=np.array([4, 15]),
+                               task_perturb_diff=np.array([2, 15]))
+    print(samples)
+    s = label_samples(samples, label_ball_launch)
+    # print(s)
+    # print(len(samples))
+    # print(len(samples[:]))
+    # print(samples[:][:, [2, 3, 6, 7]])
+    # print(samples[0])
+    # s = sbert_model.encode(["much higher"])
+    print(s)
